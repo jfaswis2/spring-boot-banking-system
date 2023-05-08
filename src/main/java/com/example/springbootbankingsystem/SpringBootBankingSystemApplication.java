c3ad86c28100c64e94499136c556e92700173650
Jhon Ferney Aristizabal Sanchez <jfaswis2@gmail.com>
2023-05-08 21:51:06 +0200

    first commit and add db config with PostgreSQL

---
+++ src/main/java/com/example/springbootbankingsystem/SpringBootBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBankingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBankingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootbankingsystem/SpringBootBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBankingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
